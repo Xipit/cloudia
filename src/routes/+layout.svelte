c843319dedf9354350fc4f6105ee44bba3cd8c0d
Hanno Witzleb <hanno2604@gmx.de>
2023-04-06 19:40:37 +0200

    adds working foundation for supabase connection

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,26 @@
-<script>
+<script lang="ts">
 	import Header from './Header.svelte';
 	import './styles.css';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import type { LayoutData } from './$types';
+
+	// AUTHENTICATION 
+	export let data: LayoutData;
+
+	$: ({ supabase, session } = data);
+
+	onMount(() => {
+		const {
+			data: { subscription },
+		} = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at){
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => subscription.unsubscribe();
+	})
 </script>
 
 <div class="app">
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import type { UserConfig } from 'vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+	//preprocessor: [vitePreprocess()]
 };
 
 export default config;
