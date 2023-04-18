5542aad8a91a4a513a77006a560dea93af22429c
Elisa <s3004389@ba-sachsen.de>
2023-04-18 13:54:36 +0200

    Erstellung neues Bürger-Menü (Svelte-Kit-Anim.)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { Hamburger } from 'svelte-hamburgers';
+    import Menu from './Menu.svelte';
+
+    let open:boolean;
+</script>
+
+<div class="icon">
+    <Hamburger
+    bind:open
+    --color="black" />
+
+    <Menu bind:open />
+</div>
+
+
+
+
+<style>
+    .icon {
+        position: absolute;
+        left: 1.25em;
+    }
+</style>
