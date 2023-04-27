91c13eea6ce7db4c0b3298103c87d406fa38f436
Simeon Baumann <sim0baum0eon@gmail.com>
2023-04-27 15:10:53 +0200

    adds Chewy and AtkinsonHyperlegible as local fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,6 @@
 	 <!-- The following lines are just temporary  -->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true">
-	<link href="https://fonts.googleapis.com/css2?family=Chewy&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 
