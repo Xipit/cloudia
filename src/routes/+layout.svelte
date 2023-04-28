a29fa7fedae18e258f8a99fa348e9bd6d02cb0ed
Simeon Baumann <sim0baum0eon@gmail.com>
2023-04-28 18:56:05 +0200

    removes google api reference

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,3 @@
-<head>
-	 <!-- The following lines are just temporary  -->
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-</head>
-
 <script lang="ts">
 	import Header from './Header.svelte';
 	import '../app.css';
