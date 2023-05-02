5b77037293f3ed65aef8b3e96dd4a45840f89e7e
jennyb106 <jy.berger36@gmail.com>
2023-05-02 16:27:47 +0200

    refactor storm weather background into its own component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,10 +17,9 @@
 	import type { LayoutData } from './$types';
 	import Menu from './Menu.svelte';
 	import { Hamburger } from 'svelte-hamburgers';
+	import Storm from './(weather-backgrounds)/storm.svelte';
 
-	import layer1 from '$lib/assets/img/layer1.svg';
-	import layer2 from '$lib/assets/img/layer2.svg';
-	import layer3 from '$lib/assets/img/layer3.svg';
+	
 
 	// AUTHENTICATION 
 	export let data: LayoutData;
@@ -41,10 +40,12 @@
 
 </script>
 
-<!--background cloud layers-->
-<img src={layer1} class="clouds" id="layer1" alt="moving clouds">
-<img src={layer2} class="clouds" id="layer2" alt="moving clouds">
-<img src={layer3} class="clouds" id="layer3" alt="moving clouds">
+<!--
+	to display different weather backgrounds add a switch statement
+	that looks current weather condition to determent the right component
+-->
+
+<Storm />
 
 <div class="app">
 	<Header bind:isLoggedIn={isLoggedIn}/>
@@ -75,37 +76,5 @@
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
-	/*cloud layers*/
-	.clouds {
-    	position: absolute;
-    	filter: drop-shadow(6px 10px 3px rgb(0 0 0 / 0.4));
-		min-width: 2000px;
-	}
-	#layer1 {
-		animation: cloud-movement1 5s linear infinite alternate;
-		/*change color svg???, https://stackoverflow.com/questions/22252472/how-can-i-change-the-color-of-an-svg-element/53336754#53336754
-		filter: invert(99%) sepia(3%) saturate(377%) hue-rotate(208deg) brightness(116%) contrast(100%);*/
-		z-index: -20;
-	}
-	#layer2 {
-		z-index: -50;
-		animation: cloud-movement2 5s linear infinite alternate;
-	}
-	#layer3 {
-		z-index: -100;
-		animation: cloud-movement3 5s linear infinite alternate;
-	}
-	/*animation cloud layers*/
-	@keyframes cloud-movement1 {
-		from {top: -95px; right:-40px;}
-		to {top: -95px; right: 0px;}
-	}
-	@keyframes cloud-movement2 {
-		from {top: -35px; right:0px;}
-		to {top: -35px; right: -30px;}
-	}
-	@keyframes cloud-movement3 {
-		from {top: 0px; right:-15px;}
-		to {top: 0px; right: 10px;}
-	}
+	
 </style>
