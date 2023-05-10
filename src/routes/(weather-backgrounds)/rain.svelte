37da0036b74a52365cc1533725f1efb4d18b7fdd
jennyb106 <jy.berger36@gmail.com>
2023-05-10 12:46:37 +0200

    rain function fix

--- src/routes/(weather-backgrounds)/rain.svelte
+++ src/routes/(weather-backgrounds)/rain.svelte
@@ -1,11 +1,12 @@
-<script>
-
-	import { fromLocalStorage } from "$lib/js/util/localStorageWrapper";
-
+<script lang="ts">
+	import RainDrop from "./raindrop.svelte";
 </script>
 
-<div id="rain" class="background-images"> 
-    <p>HI</p>
+<div class="rain-container" > 
+    
+    {#each { length: 100 } as _, i}
+        <RainDrop />
+    {/each}
 </div>
 
 
@@ -20,21 +21,5 @@
 		z-index: -20;
 	}
 
-    i {
-        position: fixed;
-        height: 200px;
-        background: linear-gradient(transparent, white);
-        border-bottom-left-radius: 5px;
-        border-bottom-right-radius: 5px;
-        animation: raining;
-    }
-    i:nth-child(3n + 1) {
-        background: linear-gradient(transparent, grey);
-    }
-
-    @keyframes raining {
-        0% {transform: translateY(-200px)}
-        100% {transform: translateY(calc(100vh + 200px))}
-    }
 
 </style>
---
+++ src/routes/(weather-backgrounds)/raindrop.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    let size = Math.random() * 6;
+    let positionX = Math.random() * 100;
+    let delay = Math.random() * -20;
+    let duration = Math.random() * 8;
+    //stops rain from falling too quickly, because of short duration of the animation
+    if (duration < 3) {     //duration < 1 --> heavy rain
+        duration += 10;
+    }
+</script>
+
+<i class="raindrop" style="
+    width: {size}px; 
+    left: {positionX}vw; 
+    animation-delay: {delay}s; 
+    animation-duration: {duration}s; 
+    ">
+
+</i>
+
+<style lang="scss">
+
+    /*RAIN*/
+    .raindrop {
+        position: fixed;
+        height: 100px;
+        background: linear-gradient(transparent, white);
+        border-bottom-left-radius: 5px;
+        border-bottom-right-radius: 5px;
+        animation: raining infinite;
+        z-index: -10;
+
+        display:block;
+    }
+
+    .raindrop:nth-child(3n + 1) {
+        background: linear-gradient(transparent, grey);
+    }
+    
+    @keyframes raining {
+        0% {transform: translateY(-200px) }
+        100% {transform: translateY(calc(100vh + 200px))}
+    }
+</style>
